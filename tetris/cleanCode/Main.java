980286edf53c5e70026c850cd420470fbf83a156
Schenatto <lucas.schenatto@sap.com>
2017-08-25 11:10:57 -0300

    remove unecessary Game folder

---
+++ tetris/cleanCode/Main.java
@@ -0,0 +1,11 @@
+package cleanCode;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Tetris game = new Tetris();
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+
+    }
+}
